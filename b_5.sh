a9c1723dde73af34edd3234ed87b524fb45d77d8
Marcelo Debailleux <hendrixexp89@gmail.com>
2023-07-10 20:47:22 -0300

    b_5 terminado

--- b_5.sh
+++ b_5.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+[ $# -ne 3 ] && echo "Ingrese 3 args : DIA MES ANIO" && exit 1
+
+DIA=$1
+MES=$2
+ANIO=$3
+
+date -d "$MES/$DIA/$ANIO" >  /dev/null 2>&1 && echo "Fecha VALIDA" || echo "Fecha INVALIDA"
+
+exit 0
+
+
